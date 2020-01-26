85c3436ca03c8d825e359d04628747f3e83c7c8a
Moritz Cremer <31781343+autonomobil@users.noreply.github.com>
2020-01-26 21:54:02 +0100

    more changes

--- pomdp_test2.jl
+++ pomdp_test2.jl
@@ -1,25 +1,37 @@
 #!/usr/bin/env julia
 module ika_decision_making
+include("utils.jl")
 
-ENV["PYTHON"] = "/usr/bin/python2.7"
 using Conda
 using RobotOS # ROS stuff for julia
 
 # import ros messages
 @rosimport geometry_msgs.msg:Point, Pose2D
-@rosimport definitions.msg:IkaEgoState, IkaObject, IkaObjectList, IkaRouteStatus, IkaLaneletRoute, IkaActionSequence
+@rosimport definitions.msg:IkaEgoState,
+IkaObject,
+IkaObjectList,
+IkaRouteStatus,
+IkaLaneletRoute,
+IkaActionSequence,
+IkaObjectListPrediction,
+IkaObjectPrediction
 rostypegen(@__MODULE__)
-import .geometry_msgs.msg:Point, Pose2D
-import .definitions.msg:IkaEgoState, IkaObject, IkaObjectList, IkaRouteStatus, IkaLaneletRoute, IkaActionSequence
+import .geometry_msgs.msg: Point, Pose2D
+import .definitions.msg:
+    IkaEgoState,
+    IkaObject,
+    IkaObjectList,
+    IkaRouteStatus,
+    IkaLaneletRoute,
+    IkaActionSequence,
+    IkaObjectListPrediction,
+    IkaObjectPrediction
 
 using POMDPs
 using POMDPModelTools
 using ARDESPOT
 # using POMCPOW
-# using POMDPSimulators
 # using POMDPPolicies
-# using POMDPPolicies
-# using POMDPSimulators
 # using ParticleFilters
 
 using StaticArrays
@@ -32,44 +44,61 @@ using LinearAlgebra
 ## Lanelet2 stuff
 # using Cxx
 # cxx""" #include "/home/cremer/git/automated-driving-platform/ros/src/perception/processing/route_planning/lanelet2/lanelet2_core/include/lanelet2_core/primitives/Lanelet.h" """
-# does not work because of include eigen, boost, etc. in header of lanelet source files
+# Does not work because of include eigen, boost, etc. in header of lanelet source files... so calculating frenet-coordinates manually?
+# route is available as vector of 3D-points(geometry_msgs.msg:Point)
 
-## State space
-const ego_state = SVector{3,Float64} # s, d, v
-const obj_states = SMatrix{(10, 4),Float64}  # s, d, v, r  | states for 10 nearest objects
+##############
+# State space
+const EgoState = SVector{3,Float64} # s, d, v
+const ObjStates = SMatrix{(10, 4),Float64}  # s, d, v, r  | states for 10 nearest objects
 
-struct State{ego_state,obj_states}
-    ego::ego_state
-    objs::obj_states
+struct State{EgoState, ObjStates}
+    ego::EgoState
+    objs::ObjStates
 end
 
+##############
 ## Observation space
-const ego_observ = SVector{3,Float64} # s, d, v
-const objs_observ = SMatrix{(10, 3),Float64} # x, y, v  | observations for 10 nearest objects
+const EgoObserv = SVector{3,Float64} # s, d, v
+const ObjObservs = SMatrix{(10, 3),Float64} # x, y, v  | observations for 10 nearest objects
 
-struct Observation{ego_observ,objs_observ}
-    ego::ego_observ
-    objs::objs_observ
+struct Observation{EgoObserv, ObjObservs}
+    ego::EgoObserv
+    objs::ObjObservs
 end
 
+##############
+# Action space
 #  3x3 lat/lon acc + 1 neutral = 10 available actions
-# 1 2 3
-# 4 5 6
-# 7 8 9
-# +1.5|-0.5     1.5|0    1.5|+0.5
-#    0|-0.5       0|0      0|+0.5
-# -1.5|-0.5    -1.5|0   -1.5|+0.5
+#     1           2        3
+#     4           5        6
+#     7           8        9
+# 
+# +1.5|-0.5    1.5|0    1.5|+0.5
+#    0|-0.5      0|0      0|+0.5
+# -1.5|-0.5   -1.5|0   -1.5|+0.5
 
-action_matrix = @SMatrix [  (1.5, -0.5)     (1.5, 0.)   (1.5, 0.);
-                            (0., -0.5)      (0., 0.)    (0., 0.5);
-                            (-1.5, -0.5)    (-1.5, 0.)  (-1.5, 0.5)]
+action_matrix = @SMatrix [
+    (1.5, -0.5) (1.5, 0.0) (1.5, 0.0)
+    (0.0, -0.5) (0.0, 0.0) (0.0, 0.5)
+    (-1.5, -0.5) (-1.5, 0.0) (-1.5, 0.5)
+]
 
 # ? const action_sequence = SVector{40,Action} # 40 actions @ 4Hz = 10 seconds planning horizon
 
-struct myPOMDP <: POMDP{State,Symbol,Observation} # POMDP{State, Action, Observation}
-end
+struct myPOMDP <: POMDP{State,Symbol,Observation} end
 
-POMDPs.actions(pomdp::myPOMDP) = [:faster_left, :faster, :faster_right, :neutral_left, :neutral, :neutral_right, :slower_left, :slower, :slower_right]
+POMDPs.actions(pomdp::myPOMDP) = [
+    :faster_left,
+    :faster,
+    :faster_right,
+    :neutral_left,
+    :neutral,
+    :neutral_right,
+    :slower_left,
+    :slower,
+    :slower_right,
+]
 
 function POMDPs.actionindex(pomdp::myPOMDP, a::Symbol)
     if a == :faster_left
@@ -92,44 +121,9 @@ function POMDPs.actionindex(pomdp::myPOMDP, a::Symbol)
         return 9
     end
     error("invalid action: $a")
-end;
-
-function getAcclerationValues(a::Symbol)
-    acc_lon = 0.0
-    acc_lat = 0.0
-    if a == :faster_left
-        acc_lon = action_matrix[1,1][1]
-        acc_lat = action_matrix[1,1][2]
-    elseif a == :faster
-        acc_lon = action_matrix[1,2][1]
-        acc_lat = action_matrix[1,2][2]
-    elseif a == :faster_right
-        acc_lon = action_matrix[1,3][1]
-        acc_lat = action_matrix[1,3][2]
-    elseif a == :neutral_left
-        acc_lon = action_matrix[2,1][1]
-        acc_lat = action_matrix[2,1][2]
-    elseif a == :neutral
-        acc_lon = action_matrix[2,2][1]
-        acc_lat = action_matrix[2,2][2]
-    elseif a == :neutral_right
-        acc_lon = action_matrix[2,3][1]
-        acc_lat = action_matrix[2,3][2]
-    elseif a == :slower_left
-        acc_lon = action_matrix[3,1][1]
-        acc_lat = action_matrix[3,1][2]
-    elseif a == :slower
-        acc_lon = action_matrix[3,2][1]
-        acc_lat = action_matrix[3,2][2]
-    elseif a == :slower_right
-        acc_lon = action_matrix[3,3][1]
-        acc_lat = action_matrix[3,3][2]
-    else
-        error("invalid action: $a")
-    end
-    return (acc_lon, acc_lat)
 end
 
+##############
 # Generative Interface
 # example:
 # function POMDPs.gen(m::MyPOMDP, s, a, rng)
@@ -149,111 +143,85 @@ function POMDPs.gen(m::myPOMDP, s::State, a::Symbol, rng)
     ## next State sp
     # ego state
     # TODO: this is not accurate because v_r can point away from route, but for ok now
-   	s_r = s.ego_state[1] + 0.25 * s.ego_state[3] + 0.5 * 0.25 * 0.25 * acc_lon; # distance along route
-   	d_r = s.ego_state[2] + 0.5 * 0.25 * 0.25 * acc_lat;                         # distance perpendicular to route
-    v_r = s.ego_state[3] + 0.25 * sqrt(acc_lon * acc_lon + acc_lat * acc_lat);  # velocity on route
-    
-    state_ego = SVector(s_r, d_r, v_r) # s, d, v
+    s_r = s.ego[1] + 0.25 * s.ego[3] + 0.5 * 0.25 * 0.25 * acc_lon # distance along route
+    d_r = s.ego[2] + 0.5 * 0.25 * 0.25 * acc_lat                         # distance perpendicular to route
+    v_r = s.ego[3] + 0.25 * sqrt(acc_lon * acc_lon + acc_lat * acc_lat)  # velocity on route
+
+    new_state_ego = SVector(s_r, d_r, v_r) # s, d, v
 
     # object states
-    state_objs = @MMatrix zeros(size(s.obj_states, 1), 4) # s, d, v, r  | states for 10 nearest objects
-    for i = 1:size(s.obj_states, 1)
-        if(s.obj_states[i].s != 0. 
-            && s.obj_states[i].d != 0.
-            && s.obj_states[i].v != 0. 
-            && s.obj_states[i].r != 0.)
-
+    new_state_objs = @MMatrix zeros(size(s.objs, 1), 4) # s, d, v, r  | states for 10 nearest objects
+    for i in 1:size(s.objs, 1)
+        if (
+            s.objs[i].s != 0.0 &&
+            s.objs[i].d != 0.0 && s.objs[i].v != 0.0 && s.objs[i].r != 0.0
+        )
             # TODO: get conflict-zone-interaction based acceleration a_k 
-            a_k = 0;
-            state_objs[i].s = s.obj_states[i].s + 0.25 * s.obj_states[i].v + 0.5 * 0.25 * 0.25 * a_k
-            state_objs[i].d = s.obj_states[i].d  
-            state_objs[i].v = s.obj_states[i].v + 0.25 * a_k
+            a_k = 0.
+            new_state_objs[i].s = s.objs[i].s + 0.25 * s.objs[i].v + 0.5 * 0.25 * 0.25 * a_k
+            new_state_objs[i].d = s.objs[i].d
+            new_state_objs[i].v = s.objs[i].v + 0.25 * a_k
 
             # TODO: Somehow calculate route probability via features (position error, v-vref error)
-            state_objs[i].route_id = s.obj_states[i].route_id;
+            new_state_objs[i].route_id = s.objs[i].route_id
         end
     end
 
-    sp = State(state_ego, state_objs)
+    sp = State(new_state_ego, new_state_objs)
 
 
     ### OBSERVATION MODEL
     ## ego observation = ego state for now
-    obser_ego = SVector(state_ego[1], state_ego[2], state_ego[3]) # s, d, v # no noise yet
+    obser_ego = SVector(new_state_ego[1], new_state_ego[2], new_state_ego[3]) # s, d, v # no noise yet
 
     # objects observation
     # TODO: Somehow generate x,y from route id and s and d 
     # -> Probably Lanelet2-functions needed, fromArccordinates(route, ...)
-    obser_objs = @MMatrix zeros(size(sp.obj_states, 1), 3)
-    for i = 1:size(sp.obj_states, 1) 
+    obser_objs = @MMatrix zeros(size(sp.objs, 1), 3)
+    for i in 1:size(sp.objs, 1)
         # obser_objs[i, 1] = obj.fPosX 
         # obser_objs[i, 2] = obj.fPosY
         # obser_objs[i, 3] = sqrt(obj.fAbsVelX * obj.fAbsVelX + obj.fAbsVelY * obj.fAbsVelY)
     end
 
     o = Observation(obser_ego, obser_objs)
-    
+
     # reward model
     r = 0.0
     # TODO: check Collision! --> negative reward if collision or very near object
     # TODO: get current v_ref of lanelet, from route_status_.v_max 
     v_ref = 50.0 / 3.6
-    
+
     # if v is not v_ref
-    r -= abs(v_ref - state_ego.v);
+    r -= abs(v_ref - new_state_ego.v)
 
     # if we go in direction of destination
-    r += state_ego.s - s.ego_state[1];
+    r += new_state_ego.s - s.ego[1]
+
+    # if we are not nn the middle line 
+    r -= abs(new_state_ego.d) * 0.2
 
     # return (sp= #=new state=#, r= #=reward=#, o= #=observation=#)
     return (sp = sp, o = o, r = r)
 end
 
-
-# https://github.com/JuliaPOMDP/POMDPExamples.jl/blob/master/notebooks/Defining-a-POMDP-with-the-Generative-Interface.ipynb 
-# Initial state distribution
-
-# The final task for describing the POMDP is specifying the initial state distribution, which can be accomplished by implementing a method of initialstate_distribution.
-
-# For this example, the baby always starts in the the "not hungry" state (false). This is implemented using the Deterministic distribution from POMDPModelTools. The distribution returned by initialstate_distribution can be any object that implements all or part of the POMDPs.jl distribution interface.
-
-# Alternatively, one may implement initialstate(m::BabyPOMDP, rng::AbstractRNG), to generate initial states, but many solvers expect at least a sampleable model of the initial state distribution, so initialstate_distribution is preferred.
-# In [4]:
-
-# POMDPs.initialstate_distribution(m::BabyPOMDP) = Deterministic(false)
-
-function POMDPs.initialstate_distribution(m::myPOMDP) 
+function POMDPs.initialstate_distribution(m::myPOMDP)
     example_ego_state = SVector(280.34234, 1.1, 13.2)
-    example_obj_states = @SMatrix [ 20. -0.5 1.5 1.;
-                                15. 0.5 12. 2.;
-                                11. -0.6 11. 1. ;
-                                10. 0.3 10. 0.5;
-                                0. 0. 0. 0.;
-                                0. 0. 0. 0.;
-                                0. 0. 0. 0.;
-                                0. 0. 0. 0.;
-                                0. 0. 0. 0.]
+    example_obj_states = @SMatrix [
+        20.0 -0.5 1.5 1.0
+        15.0 0.5 12.0 2.0
+        11.0 -0.6 11.0 1.0
+        10.0 0.3 10.0 0.5
+        0.0 0.0 0.0 0.0
+        0.0 0.0 0.0 0.0
+        0.0 0.0 0.0 0.0
+        0.0 0.0 0.0 0.0
+        0.0 0.0 0.0 0.0
+    ]
     example_state = State(example_ego_state, example_obj_states)
     return Deterministic(example_state)
 end
 
-# function POMDPs.initialstate(m::myPOMDP, rng::AbstractRNG) 
-#     example_ego_state = SVector(280.34234, 1.1, 13.2)
-#     example_obj_states = @SMatrix [ 20. -0.5 1.5 1.;
-#                                 15. 0.5 12. 2.;
-#                                 11. -0.6 11. 1. ;
-#                                 10. 0.3 10. 0.5;
-#                                 0. 0. 0. 0.;
-#                                 0. 0. 0. 0.;
-#                                 0. 0. 0. 0.;
-#                                 0. 0. 0. 0.;
-#                                 0. 0. 0. 0.]
-#     example_state = State(example_ego_state, example_obj_states)
-
-#     # return Deterministic(example_state)
-#     return example_state
-#     # return Deterministic(false)
-# end
 
 function POMDPs.isterminal(m::myPOMDP, s::State)
     return false # TODO implement
@@ -266,14 +234,15 @@ my_pomdp = myPOMDP()
 # construct and config the pomdp solver
 # D = tree height/planning horizon
 # T_max = max time for one planning step
-solver = DESPOTSolver(bounds = (-20.0, 20.0), T_max = 1.0, default_action = :neutral, D = 40) # can work with generative interface
+solver =
+    DESPOTSolver(bounds = (-20.0, 20.0), T_max = 1.0, default_action = :neutral, D = 40) # can work with generative interface
 
 ## POMCPOW
 # solver = POMCPOWSolver(max_depth = 40, max_time = 1.0) # needs explicit interface
 
 planner = solve(solver, my_pomdp) # construct the pomdp planner
 
-belief_updater = updater(planner) 
+belief_updater = updater(planner)
 
 ################################################
 
@@ -300,17 +269,17 @@ function convertMeasurements2ObservationSpace()
     obj = get(object_list_.objects, 1, 0)
     if obj != 0
         # valid objects
-        for i = 1:size(object_list_.objects, 1) 
+        for i in 1:size(object_list_.objects, 1)
             obj = get(object_list_.objects, i, 0)
-            obs_objs[i, 1] = obj.fPosX 
+            obs_objs[i, 1] = obj.fPosX
             obs_objs[i, 2] = obj.fPosY
             obs_objs[i, 3] = sqrt(obj.fAbsVelX * obj.fAbsVelX + obj.fAbsVelY * obj.fAbsVelY)
         end
     end
-    
+
     # ego observation
     if route_status_.s isa Float64 || route_status_.s isa Float32
-        obs_ego = ego_observ(route_status_.s, route_status_.d, ego_state_.fVelocity)
+        obs_ego = EgoObserv(route_status_.s, route_status_.d, ego_state_.fVelocity)
         return Observation(obs_ego, obs_objs)
     else
         return 0
@@ -322,17 +291,18 @@ function loop(pub_action)
 
     loop_rate = Rate(1.0)
 
-    while ! is_shutdown()
+    while !is_shutdown()
         println("function loop(pub_action)")
-        
+
         world_obs = convertMeasurements2ObservationSpace()
         # How to convert observation to belief? belief_updater?
         # How to get first belief? initialstate_distribution?
-        if world_obs == 0 # no measurement yet
-            # TODO: initialstate_distribution
-            # TODO: initialstate
-            # TODO: init Belief
-            # belief = POMDPs.initialize_belief(belief_updater, )
+        if world_obs == 0 
+        # no measurement yet
+        # TODO: initialstate_distribution
+        # TODO: initialstate
+        # TODO: init Belief
+        # belief = POMDPs.initialize_belief(belief_updater, )
         else
             ## TODO: Belief updater
             ## TODO: update belief from Observation
@@ -340,7 +310,7 @@ function loop(pub_action)
             a = POMDPs.action(planner, belief)
             belief_old = belief
             belief = POMDPs.update(belief_updater, belief_old, a, observation)
-            
+
             ## TODO: get Action sequence instead of one action
 
             ## Dummy action sequence
@@ -354,20 +324,20 @@ function loop(pub_action)
             # 1 2 3
             # 4 5 6
             # 7 8 9
-            
+
             # +1.5|-0.5     1.5|0    1.5|+0.5
             #    0|-0.5       0|0      0|+0.5
             # -1.5|-0.5    -1.5|0   -1.5|+0.5
-            for i = 1:40
+            for i in 1:40
                 println(vel)
-                if vel < 10.
+                if vel < 10.0
                     a_seq.actions[i] = 2
-                    vel = vel + 0.25 * action_matrix[1,2][1]
+                    vel = vel + 0.25 * action_matrix[1, 2][1]
                 end
 
-                if vel > 10.
+                if vel > 10.0
                     a_seq.actions[i] = 8
-                    vel = vel + 0.25 * action_matrix[3,2][1]
+                    vel = vel + 0.25 * action_matrix[3, 2][1]
                 end
             end
             publish(pub_action, a_seq)
@@ -380,18 +350,28 @@ end
 function main()
     init_node("POMDP")
 
-    pub_action = Publisher{IkaActionSequence}("/ika_decision_making/actions", queue_size = 20)
+    pub_action =
+        Publisher{IkaActionSequence}("/ika_decision_making/actions", queue_size = 20)
 
-    sub_ego_state = Subscriber{IkaEgoState}("/localization/ego_state", callbackEgoState, queue_size = 20)
+    sub_ego_state =
+        Subscriber{IkaEgoState}("/localization/EgoState", callbackEgoState, queue_size = 20)
 
-    sub_object_list = Subscriber{IkaObjectList}("/fusion/ikaObjectList", callbackObjectList, queue_size = 20)
+    sub_object_list = Subscriber{IkaObjectList}(
+        "/fusion/ikaObjectList",
+        callbackObjectList,
+        queue_size = 20,
+    )
 
-    sub_route_status = Subscriber{IkaRouteStatus}("/route_planning/routeStatus", callbackRouteStatus, queue_size = 20)
+    sub_route_status = Subscriber{IkaRouteStatus}(
+        "/route_planning/routeStatus",
+        callbackRouteStatus,
+        queue_size = 20,
+    )
 
     loop(pub_action)
 end
 
-if ! isinteractive()
+if !isinteractive()
     main()
 end
 
